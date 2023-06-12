dfefad138fe2b557d6591ca7d0d26b2da6c0b3c5
swastik <swastikgorai@gmail.com>
2023-06-12 18:37:54 +0530

    added more fieldds to etsts.py

--- app/models/tests.py
+++ app/models/tests.py
@@ -10,6 +10,13 @@ class Test(BaseModel):
     name = sqlalchemy.Column(sqlalchemy.String(100))
     diag_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("diagnostics.id"))
     diag = sqlalchemy.orm.relationship("Diagnostics", back_populates="tests")
-
+    category = sqlalchemy.Column(sqlalchemy.String(300))
+    method = sqlalchemy.Column(sqlalchemy.String(300))
+    specimen = sqlalchemy.Column(sqlalchemy.String(300))
+    specimen_vol = sqlalchemy.Column(sqlalchemy.String(300))
+    run_days = sqlalchemy.Column(sqlalchemy.String(300))
+    tat = sqlalchemy.Column(sqlalchemy.String(300))
+    instructions = sqlalchemy.Column(sqlalchemy.String(300), default="NA")
+    mrp = sqlalchemy.Column(sqlalchemy.Float, default=0.0)
     
     
